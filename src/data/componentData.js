221646ad6e5d655709dffb36c0730b47dd37306b
Huzaif Shaikh <huzaifsk12@gmail.com>
2024-10-13 16:01:30 +0530

    added chips

--- src/data/componentData.js
+++ src/data/componentData.js
@@ -160,4 +160,54 @@ export const ComponentData = [
     `,
     title: "Card",
   },
+  {
+    id: 4,
+    path: "/chips",
+    code: `    
+    function Chip({ label = "chips", onClose = false }) {
+  const chips = [
+    { label: "Apple", color: "red" },
+    { label: "Banana", color: "yellow" },
+    { label: "Cherry", color: "pink" },
+    { label: "Date", color: "green" },
+    { label: "Elderberry", color: "purple" },
+    { label: "Fig", color: "orange" },
+  ];
+
+  return (
+    <div>
+      {chips && chips.map((chip) => {
+        return (
+          <div
+            key={chip.label}
+              className={\`inline-flex items-center px-3 py-1 rounded-full text-sm font-medium bg-\${chip.color}\-200 text-\${chip.color}-800\`}
+          >
+            {chip.label}
+            {onClose && (
+              <button
+                onClick={onClose}
+                className="ml-2 text-blue-600 hover:text-blue-800 focus:outline-none"
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="18"
+                  height="18"
+                  viewBox="0 0 24 24"
+                  fill="currentColor"
+                  className="icon icon-tabler icons-tabler-filled icon-tabler-circle-x"
+                >
+                  <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                  <path d="M17 3.34a10 10 0 1 1 -14.995 8.984l-.005 -.324l.005 -.324a10 10 0 0 1 14.995 -8.336zm-6.489 5.8a1 1 0 0 0 -1.218 1.567l1.292 1.293l-1.292 1.293l-.083 .094a1 1 0 0 0 1.497 1.32l1.293 -1.292l1.293 1.292l.094 .083a1 1 0 0 0 1.32 -1.497l-1.292 -1.293l1.292 -1.293l.083 -.094a1 1 0 0 0 -1.497 -1.32l-1.293 1.292l-1.293 -1.292l-.094 -.083z" />
+                </svg>
+              </button>
+            )}
+          </div>
+        );
+      })}
+    </div>
+  );
+}
+        `,
+    title: "chips",
+  },
 ];
